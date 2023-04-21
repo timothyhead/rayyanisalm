1b017ef56588d025b1617953b553f8eb32a1d76b
timothyhead <48153209+timothyhead@users.noreply.github.com>
2023-04-21 10:27:01 +0100

    attempted to fix prob;em with total

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -9,7 +9,7 @@ function CartItem( props) {
     const [changeInPrice, setChangeInPrice] = useState(props.item?.price)
 useEffect(() => {
     console.log( "props.item.isIncart", props.cart.isInCart);
-}, [])
+}, [ props.cart.isInCart])
 useEffect(() => {
     props.changeInTotalValue(changeInPrice);
     console.log("change in price");
@@ -57,7 +57,7 @@ function decrement() {
         <div className="cart-item">
             <div>
                <Button variant='danger' size="lg"  onClick={handleClick}>X</Button>
-               {/* <h1>Hello world</h1> */}
+               <h1>Hello world</h1>
                 <span className="cart-item__name">{props.item?.mealName}</span>
             </div>
             <div className="cart-item__price">{newPrice}</div>
